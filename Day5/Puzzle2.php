43530901dff72e80d215934fdef9b249619b389a
M.R.J. van de Kuilen <matthijs.van.de.kuilen@hva.nl>
2021-12-05 13:02:01 +0100

    Day 5 Puzzle 2 stuck

---
+++ Day5/Puzzle2.php
@@ -0,0 +1,79 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$input = file_get_contents("../input/Day5.txt");
+
+$inputArray = explode(PHP_EOL, $input);
+$lines = array();
+
+for ($i = 0; $i < sizeof($inputArray); $i++) {
+    $lines[$i] = preg_split('/\s+/', $inputArray[$i], -1, PREG_SPLIT_NO_EMPTY);
+}
+
+$points = array();
+foreach ($lines as $line) {
+    $points1 = explode(",", $line[0]);
+    $points2 = explode(",", $line[2]);
+    if ($points1[0] == $points2[0]) {
+        $smallestPoint = min($points1[1], $points2[1]);
+        $biggestPoint = max($points1[1], $points2[1]);
+        for ($i = $smallestPoint; $i <= $biggestPoint; $i++) {
+            $points[] = $i . "," . $points1[0];
+        }
+    } elseif ($points1[1] == $points2[1]) {
+        $smallestPoint = min($points1[0], $points2[0]);
+        $biggestPoint = max($points1[0], $points2[0]);
+        for ($i = $smallestPoint; $i <= $biggestPoint; $i++) {
+            $points[] = $points1[1] . "," . $i;
+        }
+    } elseif (($points1[0] < $points2[0]) && ($points1[1] < $points2[1])) {
+        $points[] = $points2[0] . "," . $points2[1];
+        while ($points1[0] . "," . $points1[1] != $points2[0] . "," . $points2[1]) {
+            $points[] = --$points2[0] . "," . --$points2[1];
+        }
+    } elseif (($points1[0] > $points2[0]) && ($points1[1] > $points2[1])) {
+        $points[] = $points2[0] . "," . $points2[1];
+        while ($points1[0] . "," . $points1[1] != $points2[0] . "," . $points2[1]) {
+            $points[] = ++$points2[0] . "," . ++$points2[1];
+        }
+    } elseif (($points1[0] < $points2[0]) && ($points1[1] > $points2[1])) {
+        $points[] = $points2[0] . "," . $points2[1];
+        while ($points1[0] . "," . $points1[1] != $points2[0] . "," . $points2[1]) {
+            $points[] = --$points2[0] . "," . ++$points2[1];
+        }
+    } elseif (($points1[0] > $points2[0]) && ($points1[1] < $points2[1])) {
+        $points[] = $points2[0] . "," . $points2[1];
+        while ($points1[0] . "," . $points1[1] != $points2[0] . "," . $points2[1]) {
+            $points[] = ++$points2[0] . "," . --$points2[1];
+        }
+    }
+}
+
+//for ($column = 0; $column < 10; $column++) {
+//    for ($row = 0; $row < 10; $row++) {
+//        $point = $column . "," . $row;
+//        if (array_search($point, $points)) {
+//            echo "1";
+//        } else {
+//            echo ".";
+//        }
+//    }
+//    echo "<br>";
+//}
+
+$totalOverlappingPoints = array_count_values($points);
+
+$totalAmountOfOverlappingPoints = 0;
+
+foreach ($totalOverlappingPoints as $overlappingPoint) {
+    if ($overlappingPoint > 1) {
+        $totalAmountOfOverlappingPoints++;
+    }
+}
+
+echo '<pre>';
+var_dump($totalAmountOfOverlappingPoints);
+echo '</pre>';
